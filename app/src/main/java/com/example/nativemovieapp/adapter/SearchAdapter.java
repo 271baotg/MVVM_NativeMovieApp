83970e67e71bc4630772373179badef209757289
Chánh Nguyễn <21521883@gm.uit.edu.vn>
2023-04-05 23:15:57 +0700

    Chanh

--- app/src/main/java/com/example/nativemovieapp/adapter/SearchAdapter.java
+++ app/src/main/java/com/example/nativemovieapp/adapter/SearchAdapter.java
@@ -10,17 +10,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.chaek.android.RatingBar;
 import com.example.nativemovieapp.Api.Credential;
 import com.example.nativemovieapp.Model.Movie;
+import com.example.nativemovieapp.Model.RoundedCornerTransformation;
 import com.example.nativemovieapp.R;
 import com.example.nativemovieapp.viewmodel.SearchViewModels;
 import com.squareup.picasso.Picasso;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static com.example.nativemovieapp.Api.LiveDataProvider.movieListFinal;
 
-public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchViewHolder> implements Filterable {
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchViewHolder> {
     private List<Movie> mdata;
     private List<Movie> mListOld;
      Context context;
@@ -51,10 +56,10 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchView
         Movie movie = mdata.get(position);
 
         if(movie==null)return;
-        Picasso.get().load(Credential.imgBaseUrl+movie.getImageURL()).fit().into(holder.searchImage);
+        Picasso.get().load(Credential.imgBaseUrl+movie.getPoster_path()).transform(new RoundedCornerTransformation(32, 0)).fit().into(holder.searchImage);
         holder.searchTitle.setText(movie.getTitle());
         holder.searchScore.setText(String.valueOf(movie.getVote_average()));
-
+        holder.conTent.setText(movie.getOverview());
         float rating = movie.getVote_average();
 
 // Chuyển đổi điểm đánh giá thành số sao tương ứng
@@ -75,7 +80,16 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchView
 
 // Thực hiện đánh giá bằng cách đặt số sao cho đúng
         holder.ratingBar.setScore(starCount);
-        holder.year.setText(movie.getRelease_date());
+        try {
+            // Chuyển chuỗi release_date thành đối tượng Date
+            Date date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(movie.getRelease_date());
+            // Định dạng lại đối tượng Date để lấy ra năm
+            String year = new SimpleDateFormat("yyyy", Locale.getDefault()).format(date);
+            // Hiển thị năm lên TextView
+            holder.year.setText("("+year+")");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -97,6 +111,7 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchView
         private TextView searchScore;
         private TextView year;
         private RatingBar ratingBar;
+        private TextView conTent;
         public SearchViewHolder( @NotNull View itemView) {
             super(itemView);
 
@@ -105,37 +120,7 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchView
             searchScore = itemView.findViewById(R.id.search_score);
             year = itemView.findViewById(R.id.movie_year);
             ratingBar = itemView.findViewById(R.id.movie_rating);
+            conTent = itemView.findViewById(R.id.search_content);
         }
-    }
-    @Override
-    public Filter getFilter() {
-        return new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence charSequence) {
-                String strSearch = charSequence.toString();
-                if(strSearch.isEmpty())
-                {
-                    mdata=null;
-                }
-                else{
-                    List<Movie> list = new ArrayList<>();
-                    for (Movie item:movieListFinal){
-                        if(item.getTitle().toLowerCase().contains(strSearch)){
-                            list.add(item);
-                        }
-                    }
-                    mdata=list;
-                }
-                FilterResults filterResults = new FilterResults();
-                filterResults.values=mdata;
-                return filterResults;
-            }
-
-            @Override
-            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-                mdata=(List<Movie>)filterResults.values;
-                notifyDataSetChanged();
-            }
-        };
     }
 }
